00c25820b384c9e723a71f3fb5e7d42a3385be54
Rohit Vishwakarma <rohit8840@gmail.com>
2024-03-18 21:01:56 +0530

    commiting

--- src/LudoBoard.jsx
+++ src/LudoBoard.jsx
@@ -5,22 +5,31 @@ const LudoBoard = () => {
  let [moves,setMoves]=useState({blue :0, yellow :0,red :0,green :0});
  let [count ,setCount]=useState(0);
 
- function updateMoves(){
-  moves.blue+=1;
+ function updateBlue(){
+  // moves.blue+=1;
   console.log(` moves.blue =${moves.blue}`);
-  setMoves({...moves});   // here it is an spread operator which means the copy of 
+  setMoves({...moves,blue:moves.blue+1});   // here it is an spread operator which means the copy of 
   // object that be have sent out 
 
  }
+
+ function updateYellow(){
+  // moves.blue+=1;
+  console.log(` moves.blue =${moves.yellow}`);
+  setMoves({...moves,yellow:moves.yellow+1});   // here it is an spread operator which means the copy of 
+  // object that be have sent out 
+
+ }
+
 
   return (
     <div>
         <p>Game begins </p>
  <div>
   <p>Blue moves={moves.blue}</p>
-  <button style={{backgroundColor:"blue"}} onClick={updateMoves}>+1</button>
+  <button style={{backgroundColor:"blue"}} onClick={updateBlue}>+1</button>
   <p>Yellow moves={moves.yellow}</p>
-  <button style={{backgroundColor:"yellow" , color:"black"}}>+1</button>
+  <button style={{backgroundColor:"yellow" , color:"black"}} onClick={updateYellow}>+1</button>
   <p>Green moves={moves.green}</p>
   <button style={{backgroundColor:"green"}}>+1</button>
   <p>Red moves={moves.red}</p>
